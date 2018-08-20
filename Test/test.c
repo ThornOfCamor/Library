3050e8f6662597dcc79c7eded0c5e3f75dd825ab
ThornOfCamor <himangshu67@hotmail.com>
2018-08-20 06:14:38 +0530

    Added lines in test.c

--- Test/test.c
+++ Test/test.c
@@ -9,5 +9,6 @@ int main(){
 	printf("Testing factorial of 10: %lld\n", fact(10));
 	printf("Testing noOfOccurrences a = {1, 1, 1, 2, 3, 4}: %d\n", noOfOccurrences(a, 6, 2));
 	printf("Testing arrayOfDigits and AtoN: %lld\n", AtoN(arrayOfDigits(34567, &i), 5));
+	printf("Testing sqrtN for 132: %ld\n", sqrtN(132)*(sqrtN(132)+1));
 	return 0;
 }
